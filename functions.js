9cac043a8bbe14c16348271facc5491b9d8b6ff7
Is_Tree_Home <lsm3645@g.skku.edu>
2020-08-12 08:32:43 +0900

    Re Start

--- functions.js
+++ functions.js
@@ -1,18 +1,15 @@
-const frames = document.querySelectorAll(".toDoList"),
-    lB = document.querySelector(".lB"),
-    rB = document.querySelector(".rB"),
-    mapForm = document.querySelector("#map");
+var container = document.querySelector(".map"); //지도를 담을 영역의 DOM 레퍼런스
 
-let mapOptions = {
-    center: new kakao.maps.LatLng(35.82626220490655, 128.81856295258535),
-    level: 3
-}
+var options = { //지도를 생성할 때 필요한 기본 옵션
+    center: new kakao.maps.LatLng(35.82626220490655, 128.81856295258535), //지도의 중심좌표.
+    level: 3 //지도의 레벨(확대, 축소 정도)
+};
 
-let map = new kakao.maps.Map(mapForm, mapOptions);
+var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
-var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커이미지의 주소입니다
-    imageSize = new kakao.maps.Size(44, 50), // 마커이미지의 크기입니다
-    imageOption = {offset: new kakao.maps.Point(22, 50)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+var imageSrc = "./images/marker_red.png", // 마커이미지의 주소입니다
+    imageSize = new kakao.maps.Size(52, 52), // 마커이미지의 크기입니다
+    imageOption = {offset: new kakao.maps.Point(26, 26)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
 // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
 var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
@@ -29,8 +26,8 @@ marker.setMap(map);
 
 // 커스텀 오버레이에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
 var content = '<div class="customoverlay">' +
-    '  <a href="https://map.kakao.com/link/map/90820395" target="_blank">' +
-    '    <span class="title"> 태봉정밀 </span>' +
+    '  <a href="https://map.kakao.com/link/to/90820395" target="_blank">' +
+    '    <span class="title">태봉정밀</span>' +
     '  </a>' +
     '</div>';
 
@@ -44,44 +41,3 @@ var customOverlay = new kakao.maps.CustomOverlay({
     content: content,
     yAnchor: 1
 });
-
-function handleRC()
-{
-    frames.forEach(frame=>
-    {
-        if(Number(frame.className.split(" ")[1][1]) === 1)
-        {
-            frame.classList.add(`f${frames.length}`);
-        }
-        else
-        {
-            frame.classList.add(`f${Number(frame.className.split(" ")[1][1])-1}`);
-        }
-        frame.classList.remove(frame.className.split(" ")[1]);
-    })
-}
-
-function handleLC()
-{
-    frames.forEach(frame=>
-    {
-        if(Number(frame.className.split(" ")[1][1]) === frames.length)
-        {
-            frame.classList.add(`f1`);
-        }
-        else
-        {
-            frame.classList.add(`f${Number(frame.className.split(" ")[1][1])+1}`);
-        }
-        frame.classList.remove(frame.className.split(" ")[1]);
-    })
-}
-
-function init()
-{
-    lB.addEventListener("click", handleLC);
-    rB.addEventListener("click", handleRC);
-}
-
-init();
-setInterval(handleRC, 5000);
