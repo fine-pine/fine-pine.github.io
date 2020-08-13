f89711f7208b4ca6a2b86561f6121a16e318e2f7
Is_Tree_Home <lsm3645@g.skku.edu>
2020-08-13 22:44:00 +0900

    Day 2

--- functions.js
+++ functions.js
@@ -8,8 +8,8 @@ var options = { //지도를 생성할 때 필요한 기본 옵션
 var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
 var imageSrc = "./images/marker_red.png", // 마커이미지의 주소입니다
-    imageSize = new kakao.maps.Size(52, 52), // 마커이미지의 크기입니다
-    imageOption = {offset: new kakao.maps.Point(26, 26)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+    imageSize = new kakao.maps.Size(64, 54), // 마커이미지의 크기입니다
+    imageOption = {offset: new kakao.maps.Point(27, 54)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
 // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
 var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
@@ -26,7 +26,7 @@ marker.setMap(map);
 
 // 커스텀 오버레이에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
 var content = '<div class="customoverlay">' +
-    '  <a href="https://map.kakao.com/link/to/90820395" target="_blank">' +
+    '  <a href="\thttps://map.kakao.com/link/map/90820395" target="_blank">' +
     '    <span class="title">태봉정밀</span>' +
     '  </a>' +
     '</div>';
